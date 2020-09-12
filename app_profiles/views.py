d7cca8ad9cab6cf4a40437390dab6d189dea2e99
Alfa Budiman <63726302+albud187@users.noreply.github.com>
2020-09-12 14:13:37 -0300

    user posts comments

--- app_profiles/views.py
+++ app_profiles/views.py
@@ -27,20 +27,19 @@ class UserPostCommentListView(ListView):
     model=Post
     template_name = 'app_forum/user_post_comments.html'
     context_object_name='posts'
-    paginate_by=20
+    # paginate_by=20
 
     def get_queryset(self):
         user = get_object_or_404(User, username=self.kwargs.get('username'))
-        AllUserCommentsList = PostComment.objects.filter(author=user)
-        AllUserCommentsPostsList=[]
+        AllUserComments = PostComment.objects.filter(author=user)
+        AllUserCommentsPostList = []
+        for comment in AllUserComments:
+            AllUserCommentsPostList.append(comment.post)
 
-        for comment in AllUserCommentsList:
-            AllUserCommentsPostsList.append(comment.post)
+        AllPosts = Post.objects.all()
+        AllPostsList = list(AllPosts)
+        for post in AllPostsList:
+            if post not in AllUserCommentsPostList:
+                AllPostsList.remove(post)
 
-        AllUserPosts = Post.objects.filter(author=user)
-
-        for post in AllUserPosts:
-            if post not in AllUserCommentsPostsList:
-                AllUserPosts.pop(post)
-
-        return AllUserPosts.filter(author=user).first()
+        return AllPostsList
